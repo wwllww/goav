3741a15cec1801d184e5b17f0a16c9b9f0c3621b
runningwang(王月通) <runningwang@tencent.com>
2024-03-13 14:43:41 +0800

    fix

--- avutil/avutil.go
+++ avutil/avutil.go
@@ -17,7 +17,6 @@ import (
 
 type (
 	Options       C.struct_AVOptions
-	Dictionary    C.struct_AVDictionary
 	AvTree        C.struct_AVTree
 	Rational      C.struct_AVRational
 	MediaType     C.enum_AVMediaType
@@ -26,48 +25,48 @@ type (
 	File          C.FILE
 )
 
-//Return the LIBAvUTIL_VERSION_INT constant.
+// Return the LIBAvUTIL_VERSION_INT constant.
 func AvutilVersion() uint {
 	return uint(C.avutil_version())
 }
 
-//Return the libavutil build-time configuration.
+// Return the libavutil build-time configuration.
 func AvutilConfiguration() string {
 	return C.GoString(C.avutil_configuration())
 }
 
-//Return the libavutil license.
+// Return the libavutil license.
 func AvutilLicense() string {
 	return C.GoString(C.avutil_license())
 }
 
-//Return a string describing the media_type enum, NULL if media_type is unknown.
+// Return a string describing the media_type enum, NULL if media_type is unknown.
 func AvGetMediaTypeString(mt MediaType) string {
 	return C.GoString(C.av_get_media_type_string((C.enum_AVMediaType)(mt)))
 }
 
-//Return a single letter to describe the given picture type pict_type.
+// Return a single letter to describe the given picture type pict_type.
 func AvGetPictureTypeChar(pt AvPictureType) string {
 	return string(C.av_get_picture_type_char((C.enum_AVPictureType)(pt)))
 }
 
-//Return x default pointer in case p is NULL.
+// Return x default pointer in case p is NULL.
 func AvXIfNull(p, x int) {
 	C.av_x_if_null(unsafe.Pointer(&p), unsafe.Pointer(&x))
 }
 
-//Compute the length of an integer list.
+// Compute the length of an integer list.
 func AvIntListLengthForSize(e uint, l int, t uint64) uint {
 	return uint(C.av_int_list_length_for_size(C.uint(e), unsafe.Pointer(&l), (C.uint64_t)(t)))
 }
 
-//Open a file using a UTF-8 filename.
+// Open a file using a UTF-8 filename.
 func AvFopenUtf8(p, m string) *File {
 	f := C.av_fopen_utf8(C.CString(p), C.CString(m))
 	return (*File)(f)
 }
 
-//Return the fractional representation of the internal time base.
+// Return the fractional representation of the internal time base.
 func AvGetTimeBaseQ() Rational {
 	return (Rational)(C.av_get_time_base_q())
 }
